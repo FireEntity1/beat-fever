c3c49921cba3d8e93c214dcea2b5dd325e940b3e
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-07-24 21:15:03 -0600

    Mapping

--- end.gd
+++ end.gd
@@ -12,18 +12,89 @@ func _ready():
 
 func _physics_process(delta):
 	beat += 0.042
-	middle(7)
-	middle(7.5)
-	middle(8)
-	middle(8.5)
-	middle(9)
-	middle(14)
-	middle(14.5)
-	middle(15)
-	middle(15.5)
-	middle(16)
-	middle(17)
-	middle(18)
+	spawn(7)
+	spawn(7.5)
+	spawn(8)
+	spawn(8.5)
+	spawn(9)
+	
+	spawn(14)
+	spawn(14.5)
+	spawn(15)
+	spawn(15.5)
+	spawn(16)
+	spawn(16.5)
+	spawn(18)
+	
+	spawn(23)
+	spawn(23.5)
+	spawn(24)
+	spawn(24.5)
+	spawn(25)
+	
+	spawn(30)
+	spawn(30.5)
+	spawn(31)
+	spawn(31.5)
+	spawn(32)
+	spawn(32.5)
+	spawn(34)
+	
+	spawn(38.5)
+	spawn(39)
+	spawn(39.5)
+	spawn(40)
+	spawn(40.45)
+	spawn(41)
+	
+	spawn(46.5)
+	spawn(47)
+	spawn(47.5)
+	spawn(48)
+	spawn(48.5)
+	spawn(50)
+	spawn(51.5)
+	
+	spawn(55)
+	spawn(55.5)
+	spawn(56)
+	spawn(56.5)
+	spawn(57)
+	
+	spawn(62.5)
+	spawn(63)
+	spawn(63.5)
+	spawn(64)
+	spawn(64.5)
+	spawn(66)
+	
+	spawn(73)
+	spawn(74.5)
+	spawn(76)
+	spawn(77.5)
+	spawn(79)
+	spawn(80)
+	spawn(81)
+	spawn(82.6)
+	spawn(84)
+	spawn(85.5)
+	spawn(87)
+	spawn(88)
+	spawn(89)
+	spawn(90.5)
+	spawn(92)
+	spawn(93.5)
+	spawn(95)
+	spawn(96)
+	spawn(96.5)
+	
+	spawn(101.5)
+	spawn(102)
+	spawn(102.5)
+	
+	
+	
+	
 
 func _process(delta):
 	get_overlapping_areas()
@@ -43,8 +114,8 @@ func _on_area_entered(area):
 func _on_bpm_timer_timeout():
 	$Music.play()
 
-func middle(fireBeat,fired=false):
-	if beat > fireBeat - 4 and beat < fireBeat-3.96:
+func spawn(fireBeat,fired=false):
+	if beat >= fireBeat - 4 and beat < fireBeat-3.96:
 		add_child(note.instantiate())
 		
 
